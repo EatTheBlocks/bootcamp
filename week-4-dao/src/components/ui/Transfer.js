6da817da554f295b88c1ed75cf441b8405c5a72e
Rahul Ravindran <rahulravindran2605@gmail.com>
2022-05-24 18:45:48 +0530

    feat(DAO): :sparkles: Transfer

--- week-4-dao/src/components/ui/Transfer.js
+++ week-4-dao/src/components/ui/Transfer.js
@@ -1,13 +1,69 @@
+import { useState } from "react";
+import { Form, Modal } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import { HiRefresh } from "react-icons/hi";
+import { DAOContract, showError } from "../../utils/common";
 const Transfer = () => {
+  const [show, setShow] = useState(false);
+
+  const [amount, setAmount] = useState(0);
+  const [recipient, setRecipient] = useState("");
+
   const transfer = () => {
-    console.log("Transfer");
+    setShow(true);
   };
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await DAOContract().transfer(amount, recipient);
+    } catch (error) {
+      showError(error);
+    }
+    handleClose();
+  };
+
   return (
-    <Button className="mx-2" onClick={transfer}>
-      <HiRefresh size={20} /> Transfer Shares
-    </Button>
+    <div>
+      <Button className="mx-2" onClick={transfer}>
+        <HiRefresh size={20} /> Transfer Shares
+      </Button>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Contribute to ETB DAO</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form onSubmit={handleSubmit}>
+            <Form.Group className="mb-2" controlId="amount">
+              <Form.Label>Amount</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Form.Group className="mb-2" controlId="recipient">
+              <Form.Label>Recipient</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter recipient address"
+                value={recipient}
+                onChange={(e) => setRecipient(e.target.value)}
+                required
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit" className="mt-2">
+              Contribute
+            </Button>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </div>
   );
 };
 
