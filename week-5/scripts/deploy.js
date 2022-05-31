05fcff2aeec48d4571f6146ccf741cb92e22a215
Rahul Ravindran <ravindranrahul@users.noreply.github.com>
2022-05-31 13:21:58 +0530

    feat(Project): :zap: Init

---
+++ week-5/scripts/deploy.js
@@ -0,0 +1,44 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+const fs = require("fs");
+
+const CONTRACT_NAME = "Ebay";
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const Contract = await hre.ethers.getContractFactory(CONTRACT_NAME);
+  const contract = await Contract.deploy();
+
+  //   await greeter.deployed();
+
+  console.log("Greeter deployed to:", contract.address);
+
+  // Write contract address to artifact
+  fs.writeFileSync(
+    __dirname +
+      "/../frontend/src/artifacts/contracts/" +
+      CONTRACT_NAME +
+      ".sol/contract-address.json",
+    JSON.stringify({ address: contract.address }, undefined, 2)
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
